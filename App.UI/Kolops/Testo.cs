1f2f8d83b2f7e686a5b544466d98d3837c6aec0a
Muhammed Talha Kavak <146962096+talhakvk@users.noreply.github.com>
2025-05-09 02:14:04 +0300

    added file in AppUI NEW

---
+++ App.UI/Kolops/Testo.cs
@@ -0,0 +1,9 @@
+﻿namespace App.UI.Kolops
+{
+    public class Testo
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int LName { get; set; }
+    }
+}
